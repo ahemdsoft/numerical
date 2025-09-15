c63f8c02fb005b195c2f791ffb427ab9071ec4c7
ahmed_soft <anasahmed4484@gmail.com>
2025-09-15 10:20:19 +0600

    Update secant.cpp

--- secant.cpp
+++ secant.cpp
@@ -11,20 +11,20 @@ double f(double x) {
 /*
 Secant Method
 Formula (from lecture slide):
-x3 = x2 - f(x2) * (x2 - x1) / (f(x2) - f(x1))
-Stopping criteria: |x_new - x_old| < tolerance
+x0 = x2 - f(x2) * (x2 - x1) / (f(x2) - f(x1))
+Stopping criteria: |x_new - x_old|/|x_new| < tolerance
 */
 double secant(double x1, double x2, double tol, int maxIter) {
     double f1 = f(x1), f2 = f(x2);
-    double x3;
+    double x0;
 
     cout << left << setw(6) << "Iter"
          << setw(12) << "x1"
          << setw(12) << "x2"
          << setw(12) << "f1"
          << setw(12) << "f2"
-         << setw(12) << "x3"
-         << setw(12) << "f(x3)"
+         << setw(12) << "x0"
+         << setw(12) << "f(x0)"
          << setw(20) << "Relative error" << endl;
 
     for (int iter = 1; iter <= maxIter; iter++) {
@@ -33,37 +33,38 @@ double secant(double x1, double x2, double tol, int maxIter) {
             return NAN;
         }
 
-        x3 = x2 - f2 * (x2 - x1) / (f2 - f1);   // secant formula
-        double f3 = f(x3);
-        double error = fabs(x3 - x2);
+        x0 = x2 - f2 * (x2 - x1) / (f2 - f1);   // secant formula
+        double f0 = f(x0);
+        double error = fabs(x0 - x2)/fabs(x0);
 
         cout << left << setw(6) << iter
              << setw(12) << x1
              << setw(12) << x2
              << setw(12) << f1
              << setw(12) << f2
-             << setw(12) << x3
-             << setw(12) << f3
+             << setw(12) << x0
+             << setw(12) << f0
              << setw(20) << error << endl;
 
-        if (fabs(f3) < tol || error < tol) break;
+        if (fabs(f0) < tol || error < tol) break;
 
         // update
         x1 = x2; f1 = f2;
-        x2 = x3; f2 = f3;
+        x2 = x0; f2 = f0;
     }
 
-    return x3;
+    return x0;
 }
 
 int main() {
-    double x1, x0 ,tol;
-    cin >> x0;
+    double x1, x2 ,tol;
+    cout<<"enter guess-1 guess-2 and error-tolerance : ";
+    cin >> x2;
     cin >> x1;
     cin>> tol;
     int maxIter = 50;
 
-    double root = secant(x0, x1, tol, maxIter);
+    double root = secant(x1, x2, tol, maxIter);
     cout << "\nApproximate root (Secant Method): " << root << endl;
     return 0;
 }
